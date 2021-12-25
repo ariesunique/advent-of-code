606ec4f901ab768ee2f74d45bdbb1cdf7b4f81a7
ariesunique <ariesunique@gmail.com>
2021-12-24 21:22:03 -0500

    Day 18 - Use tree

--- day18.py
+++ day18.py
@@ -31,83 +31,34 @@ def parse(line):
             right = stack.pop()
             left = stack.pop()
             _ = stack.pop()
-            pair=(left, right)
-            stack.append(pair)
+
+            parent = Node(f"({left},{right})")
+            parent.left = Node(left) if isinstance(left, int) else left
+            parent.right = Node(right) if isinstance(right, int) else right
+            stack.append(parent)
+
         elif ch.isdigit():
             stack.append(int(ch))
     return stack[0]
 
-class Snailfish():
-    all_snailfish = {}
-
-    def __init__(self, pair=None, parent=None, nesting=0, left_or_right=None, xchild=None, ychild=None):
-        if not pair and not xchild and not ychild:
-            print("uh oh")
-            return
-
-        if xchild and ychild:
-            self.xchild = xchild
-            self.ychild = ychild
-            self.name = "".join(random.choice(string.ascii_lowercase) for i in range(8))
-            Snailfish.all_snailfish[self.name] = self
-            return
-
-        self.x = pair[0]
-        self.y = pair[1]
-        self.xType = "int" if isinstance(self.x, int) else "list"
-        self.yType = "int" if isinstance(self.y, int) else "list"
-        self.name = "".join(random.choice(string.ascii_lowercase) for i in range(8))
-        self.parent = parent
-        self.nesting = nesting
-        self.left_or_right = left_or_right
-        self.xchild = None
-        self.ychild = None
-        if self.xType == "list":
-            self.xchild = Snailfish(self.x, parent=self.name, nesting=self.nesting+1, left_or_right="left")
-        if self.yType == "list":
-            self.ychild = Snailfish(self.y, parent=self.name, nesting=self.nesting+1, left_or_right="right")
-
-        Snailfish.all_snailfish[self.name] = self
-
-    def __add__(self, other):
-        print(f"adding {self} and {other}")
-        return Snailfish(xchild=self, ychild=other)
+class Node():
+    def __init__(self, value=None):
+        self.left = None
+        self.right = None
+        self.value = value
 
     def __str__(self):
-        if self.xchild and self.ychild:
-            res = f"\nSnailfish: {self.name}\nx is {repr(self.xchild)}\ny is {repr(self.ychild)}"
-        else:
-            res = f"\nSnailfish: {self.name}\nx is {self.x} type {self.xType}\ny is {self.y} type {self.yType};\nchild of {self.parent}\nI'm on the {self.left_or_right} \nnested level is {self.nesting}"
-        return res
+        return f"({'' if not self.left else self.left.value}, {'' if not self.right else self.right.value})"
 
     def __repr__(self):
-        if self.xchild and self.ychild:
-            return str((self.xchild, self.ychild))
-        return str((self.x, self.y))
+        return str(self)
 
-    def explode(self):
-        print("\nexploding")
-        print("parent", Snailfish.all_snailfish[self.parent])
-        number_on_the_right, sfname = self.get_number_on_the_right()
-        number_on_the_left = self.get_number_on_the_left()
-        print("right", number_on_the_right, "found in", sfname)
-        if number_on_the_right:
-            Snailfish.all_snailfish[sfname]
-        print("left", number_on_the_left)
-        
-
-    def get_number_on_the_right(self):
-        parent = Snailfish.all_snailfish[self.parent]
-        while parent:
-            if parent.yType == "int":
-                return parent.y, parent.name
-            parent = Snailfish.all_snailfish[parent.parent]
-
-    def get_number_on_the_left(self):
-        parent = Snailfish.all_snailfish[self.parent]
-        if parent.xType == "int":
-            return parent.x
-
+def inorder(root):
+    if root is None:
+        return
+    inorder(root.left)
+    print(root.value, end=' ')
+    inorder(root.right)
 
 def main():
     puzzle = Puzzle(year=2021, day=18)
@@ -115,47 +66,10 @@ def main():
     snail_list = get_input()
     #target_area = get_input(puzzle, mode="real") 
 
-    # prev_snail = Snailfish(pair=snail_list[0])
-    # for i in range(1, len(snail_list)):
-    #     next_snail = Snailfish(pair=snail_list[i])
-    #     prev_snail = prev_snail + next_snail
-    #     #break
-    # print(prev_snail)
-
-
-
-    #     print(repr(snail))
-    # print(len(Snailfish.all_snailfish))
-
-    # parse("[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]")
-
-    sf1 = Snailfish(pair=[[6,[5,[4,[3,2]]]],1])
-    print(sf1)
-    # sf2 = Snailfish(pair=[7,9])
-    # sf3 = sf1 + sf2
-
-    #print("S3 is", sf3)
-
-    # sf = Snailfish([1,2])
-    # print(sf)
-
-    # sf = Snailfish([[1,2],3])
-    # print(sf)
-
-    #print()
-    #print(Snailfish.all_snailfish)
-
-    print()
-    for x in Snailfish.all_snailfish.values():
-        if x.nesting == 4:
-            print(x)
-            x.explode()
-
-    print()
-    print(sf1)
-
-    #print()
-    #print(sf.xchild.name, sf.x)
+    for s in snail_list[1:2]:
+        inorder(s)
+        print()
+        break
 
 
 
