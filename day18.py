0dad6b0f67d2edab00bbd17b353942befdf776c0
ariesunique <ariesunique@gmail.com>
2021-12-26 22:04:51 -0500

    Part one works

--- day18.py
+++ day18.py
@@ -4,19 +4,20 @@ import string
 from pprint import pprint
 import sys
 
+
 def get_input(puzzle=None, mode="test"):
     if mode=="test":
         raw_data = """
-[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
-[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
-[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
-[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
-[7,[5,[[3,8],[1,4]]]]
-[[2,[2,2]],[8,[8,1]]]
-[2,9]
-[1,[[[9,3],9],[[9,0],[0,7]]]]
-[[[5,[7,4]],7],1]
-[[[[4,2],2],6],[8,7]]
+[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
 """.strip().splitlines()
     else:
         raw_data = puzzle.input_data.splitlines()
@@ -24,6 +25,7 @@ def get_input(puzzle=None, mode="test"):
     #print(raw_data)
     list_of_snailfish_trees = [ parse(line) for line in raw_data ]
     return list_of_snailfish_trees
+
 
 OPEN = "["
 CLOSE = "]"
@@ -80,21 +82,27 @@ class Node():
 def inorder(root, parent=None, level=0, ordered_tree=[]):
     if root is None:
         return
-    #print(f"getting order for {root}: my left is {root.left}; my right is {root.right}")
+
     inorder(root.left, parent=root.name, level=level+1, ordered_tree=ordered_tree)
-    #print(f"{root.name}, {root.value}, Level {level} ")
     ordered_tree.append((root.name,root.value,level, parent))
     inorder(root.right, parent=root.name, level=level+1, ordered_tree=ordered_tree)
     root.reset_value()
 
 
+def getMagnitude(root):
+    if root.left and root.right:
+        leftMag = getMagnitude(root.left)
+        rightMag = getMagnitude(root.right)
+        return 3 * leftMag + 2 * rightMag
+    else:
+        return root.value
+
+
 def update_neighbor(ordered_snail_tree, startindex, direction, value_to_add):
     for name, value, _, parent in ordered_snail_tree[startindex::direction]:
-        if isinstance(value, int):
+        if isinstance(value, int) and name in  Node.all_snailfish:
             node_to_update = Node.all_snailfish[name]
             node_to_update.value += value_to_add
-            #parent_node = Node.all_snailfish[parent] 
-            #parent_node.value = f"({parent_node.left},{parent_node.right})"
             break
 
 
@@ -117,10 +125,9 @@ def splitInt(ordered_snail_tree, index):
         parent_node.left = new_node
     else:
         parent_node.right = new_node
-    #parent_node.value = f"({parent_node.left},{parent_node.right})"
 
     del Node.all_snailfish[name]    
-    #print("done splitting int")
+
 
 
 def explodePair(ordered_snail_tree, index):
@@ -137,78 +144,57 @@ def explodePair(ordered_snail_tree, index):
         update_neighbor(ordered_snail_tree, index+2, 1, current_node.right.value)
 
     parent_node = Node.all_snailfish[parent]
-    #print("parent",parent_node)
+
     if parent_node.left == current_node:
         parent_node.left = Node(0)
     else:
         parent_node.right = Node(0)
-    #del ordered_snail_tree[index]
-    #print(f"Update {parent_node} value to {({parent_node.left},{parent_node.right})}")
-    #parent_node.value = f"({parent_node.left},{parent_node.right})"
-    #print(f"Now it's {parent_node}")
+
     del Node.all_snailfish[current_node.left.name]
     del Node.all_snailfish[current_node.right.name]
     del Node.all_snailfish[name]
 
 
 def reduce(snailfish):
-    done = False
-    while not done:
+    explodedAllPairs = False
+    splitAllInts = False
+    while not explodedAllPairs or not splitAllInts:
         ordered_snail_tree = []
         inorder(snailfish, ordered_tree=ordered_snail_tree)        
         for index1, (name, value, level, parent) in enumerate(ordered_snail_tree):
-            if not isinstance(value, int) and level >= 4:  
+            if not isinstance(value, int) and level >= 4 and name in Node.all_snailfish and isinstance(Node.all_snailfish[name].left.value, int) and isinstance(Node.all_snailfish[name].right.value, int):  
                 explodePair(ordered_snail_tree, index1)
                 break
         if index1 < len(ordered_snail_tree)-1: 
             continue
+        explodedAllPairs = True
         for index2, (name, value, level, parent) in enumerate(ordered_snail_tree):
             if isinstance(value, int) and value >= 10:
                 splitInt(ordered_snail_tree, index2)
+                explodedAllPairs = False
                 break
         if index2 == len(ordered_snail_tree)-1: 
-            done=True
-    # just to update the display - turn off when running on large input
-    #inorder(snailfish, ordered_tree=ordered_snail_tree)
+            splitAllInts = True
+
 
 
 def main():
     puzzle = Puzzle(year=2021, day=18)
 
-    snail_list = get_input()
-    #target_area = get_input(puzzle, mode="real") 
+    #snail_list = get_input()
+    snail_list = get_input(puzzle, mode="real") 
 
-    #print(Node.all_snailfish)
     res = snail_list[0]
     for sf in snail_list[1:]:
         #print(f"Adding {res} and {sf}")
         res = res + sf
         reduce(res)
-        # need to call again to fix the very last pair in the sequence, should that one need splitting or exploding
-        reduce(res)
         
+    ans = getMagnitude(res)
 
     print("\n",res)
-    # ordered_snail_tree = []
-    # inorder(res, ordered_tree=ordered_snail_tree)
-    # pprint(ordered_snail_tree)     
+    print(ans)
 
-    # sf1 = parse("[[[[4,3],4],4],[7,[[8,4],9]]]")
-    # sf2 = parse("[1,1]")
-    # sf3 = sf1 + sf2
-    # print(sf3)
-    # print("all snailfish", len(Node.all_snailfish))
-    # reduce(sf3)
-
-    # print("\nUpdated\n")
-    # ordered_snail_tree = []
-    # inorder(res, ordered_tree=ordered_snail_tree)
-    # pprint(ordered_snail_tree)   
-    # print("all snailfish", len(Node.all_snailfish))     
-    #pprint(Node.all_snailfish)
-
-    # for key, x in Node.all_snailfish.items():
-    #     print(f"{x.value} => Left: {x.left}; Right: {x.right}")
 
 
 
